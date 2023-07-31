ba2891c76c54ffb89fce731d12eb74bf416387d8
srosen9 <srosen@uoguelph.ca>
2023-07-31 08:06:50 -0400

    small edits and cleaning comments

--- TaskCode.R
+++ TaskCode.R
@@ -17,9 +17,10 @@ error=rnorm(100,0,150)
 #declare dog as factor
 dog <- as.factor(dog)
 
+#creating salary variable
 salary=100000+(4000*dog)+(200*kindness)+error
 
-
+#creating dataframe
 StaffData<- data.frame(
   dog,
   kindness,
@@ -28,25 +29,17 @@ StaffData<- data.frame(
 
 remove(dog,kindness,salary)
 
-#Investigating with statistical test
+#Investigating with statistical test and plots ----
 
 #Creating a pairs plot to investigate relationship
 
-ggplot(StaffData, aes(x=dog, y = salary)) + geom_boxplot(fill = 'lightblue')
+ggplot(StaffData, aes(x=dog, y = salary)) + geom_boxplot(fill = 'lightblue') + scale_x_discrete(labels=c("dog-owner", "non-dog owner")) +
+  ggtitle("Salary according to dog-ownership") +
+  theme(plot.title = element_text(hjust = 0.5))
 
-
-
-#Based on our pairs plot, we can assume a linear relationship. Let's use the lm function to assess the relationship and estimate the effects.
+#Based on our pairs plot, we can assume a linear relationship. Let's use the lm function to assess the relationship and estimate the effects. With more time, we should also assess for linear model assumptions with plots such as qqnorm.
 mod1 <- lm(salary~dog + kindness)
 summary(mod1)
 
 
-#scatterplot with regression line
-ggplot(data = StaffData, aes(x = kindness, y = kindness)) +
-  geom_point(aes(color = "blue")) +
-  geom_smooth(method = "lm") 
-
-#Note, we should also check the linear model assumptions before we can take these results
-
-#We can also use a statistical test to look at the salary differences between dog owners and staff that do not have dogs.
 
